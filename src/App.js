848541c618e6c04d7bda026fd8849490cf7621d2
Duz Dz <vuduc1611@gmail.com>
2023-05-31 14:20:48 +0700

    multi component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./views/Nav";
 
 function App() {
+  let name = "Duc";
+  let obj = { name: "Vu", age: "20" };
+  let link = "https://www.youtube.com/";
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>Hello world</div>
+        <div>
+          Hello world with {name} in {obj.name}
+        </div>
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link} target="_blank">
+          Visit youtube
+        </a>
       </header>
     </div>
   );
