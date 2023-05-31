3856cff9c3c994d56e42452e7bd35327f4690c40
Duz Dz <vuduc1611@gmail.com>
2023-05-31 13:38:34 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>Hello world</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
