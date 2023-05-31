ba9c24a88592ee84b37f2bb1eb6148eb36b77bb6
Duz Dz <vuduc1611@gmail.com>
2023-05-31 15:45:12 +0700

    State and useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./views/Nav";
+import { useState } from "react";
 
 function App() {
-  let name = "Duc";
-  let obj = { name: "Vu", age: "20" };
-  let link = "https://www.youtube.com/";
+  // let [name, setName] = useState("Duc");
+  // const [address, setAddress] = useState("");
+
+  // const handleEventClick = (event) => {
+  //   setName(address);
+  // };
+
+  // const handleOnChangInput = (event) => {
+  //   setAddress(event.target.value);
+  // };
+
+  const [name, setName] = useState("");
+  const [currentName, setCurrentName] = useState("");
+  // const handleOnChangInput = (event) => {
+  //   setName(event.target.value);
+  // };
+
+  const handleEventClick = () => {
+    setCurrentName(name);
+  };
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          Hello world with {name} in {obj.name}
-        </div>
-        <p style={{ color: "red", fontSize: "20px", marginTop: "20px" }}>
-          {JSON.stringify(obj)}
-        </p>
-        <a href={link} target="_blank">
-          Visit youtube
-        </a>
+        <div>Hello world with {currentName}</div>
+        <br />
+
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+        <button type="text" onClick={() => handleEventClick()}>
+          Click me
+        </button>
+
+        {/* <input
+          type="text"
+          value={address}
+          onChange={(event) => handleOnChangInput(event)}
+        />
+        <button type="text" onClick={(event) => handleEventClick(event)}>
+          Click me
+        </button> */}
       </header>
     </div>
   );
