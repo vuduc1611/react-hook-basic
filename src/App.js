fca36b5bd4d59f46bb336be2e3ebfa340ac833a7
Duz Dz <vuduc1611@gmail.com>
2023-05-31 16:09:51 +0700

    output list and render loop

--- src/App.js
+++ src/App.js
@@ -4,51 +4,54 @@ import Nav from "./views/Nav";
 import { useState } from "react";
 
 function App() {
-  // let [name, setName] = useState("Duc");
-  // const [address, setAddress] = useState("");
+  let [name, setName] = useState("Duc");
+  const [address, setAddress] = useState("");
 
-  // const handleEventClick = (event) => {
-  //   setName(address);
-  // };
-
-  // const handleOnChangInput = (event) => {
-  //   setAddress(event.target.value);
-  // };
-
-  const [name, setName] = useState("");
-  const [currentName, setCurrentName] = useState("");
-  // const handleOnChangInput = (event) => {
-  //   setName(event.target.value);
-  // };
-
-  const handleEventClick = () => {
-    setCurrentName(name);
+  const handleEventClick = (event) => {
+    if (!address) {
+      alert("Empty input");
+      return;
+    }
+    let newTodo = { id: "abc", title: address };
+    setTodos([...todos, newTodo]);
+    setAddress("");
   };
+
+  const handleOnChangInput = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const [todos, setTodos] = useState([
+    { id: "todo1", title: "Watching " },
+    { id: "todo2", title: "Learning " },
+    { id: "todo3", title: "Playing Game " },
+  ]);
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>Hello world with {currentName}</div>
+        <div>Hello world with {name}</div>
         <br />
+        <div className="todos-container">
+          {todos.map((todo) => {
+            return (
+              <li className="todo-child" key={todo.id}>
+                {todo.title}
+              </li>
+            );
+          })}
+        </div>
 
         <input
-          type="text"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-        <button type="text" onClick={() => handleEventClick()}>
-          Click me
-        </button>
-
-        {/* <input
           type="text"
           value={address}
           onChange={(event) => handleOnChangInput(event)}
         />
         <button type="text" onClick={(event) => handleEventClick(event)}>
           Click me
-        </button> */}
+        </button>
       </header>
     </div>
   );
